7ddde455963fd518b899f1948a3210da1a48ff8f
ya-naka <y.naitkaci@gmail.com>
2021-04-11 14:51:23 +0200

    suppression de la partie Steps

--- ProjetTuteureClient/src/components/EditMenu.js
+++ ProjetTuteureClient/src/components/EditMenu.js
@@ -6,112 +6,27 @@ import Step from "./Step"
 
 function EditMenu(props){
     const [error, setError] = useState("")
-    const [stepsInfos, setStepsInfos] = useState("")
-    const [selectedTask, setSelectedTask] = useState(props.selectedTask)
 
-    function displaySteps(){
-        const display = "";
-        for(var i =0; i < stepsInfos.length; i++){
-            display += <Step index={i} stepInfos={stepsInfos[i]} />
-        }
-        return display;
-    }
-
-    async function request(){
-        let parameters = new URLSearchParams()
-        parameters.append("IdTask",props.tasksInfos[selectedTask].IdTask);
-    
-        const options = {
-          method: 'POST',
-          body: parameters
-        }
-        const reponse = await fetch('http://localhost:80/ProjetTuteureV2/ProjetTuteureServer/get_steps', options)
-        const data = await reponse.json()
-        if(reponse.status === 200){
-          setStepsInfos(data.steps)
-        }
-        if(reponse.status === 201){
-          setStepsInfos("")
-          setError(data.message)
-        }else{
-          setError(data.message)
-        }
-      }
-
-    useEffect(() => {
-        setSelectedTask(props.selectedTask)
-        request()
-    }, [props.selectedTask])
-
-      if(props.stepInfos !== ""){
         return(
             <div id="menu-task-edit">
-        <div className="block">
-            <label>Titre</label>
-            <input type="text" placeholder={props.taskInfos.Title} />
-        </div>
-        <div className="block">
-            <label>Étapes</label>
-            <ul>
-                {displaySteps()}
-                <li className="step-container">
-                    <input type="text" className="step" placeholder="Nouvelle étape" />
-                    <div className="step-actions">
-                        <PlusIcon />
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <div className="block">
-            <label>Échéance</label>
-            <input type="text" placeholder={props.taskInfos.DeadLine} />
-        </div>
-        <div className="block">
-            <label>Notes</label>
-            <textarea placeholder={props.taskInfos.Description}></textarea>
-        </div>
-        <div className="double-buttons">
-            <button>Enregistrer</button>
-            <button className="optional">Annuler</button>
-        </div>
-    </div>
-        )
-      }
-        
-            
-              
-          
-            return (
-                <div id="menu-task-edit">
-                    <div className="block">
-                        <label>Titre</label>
-                        <input type="text" placeholder={props.taskInfos.Title} />
-                    </div>
-                    <div className="block">
-                        <label>Étapes</label>
-                        <ul>
-                            <li className="step-container">
-                                <input type="text" className="step" placeholder="Nouvelle étape" />
-                                <div className="step-actions">
-                                    <PlusIcon />
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="block">
-                        <label>Échéance</label>
-                        <input type="text" placeholder={props.taskInfos.DeadLine} />
-                    </div>
-                    <div className="block">
-                        <label>Notes</label>
-                        <textarea placeholder={props.taskInfos.Description}></textarea>
-                    </div>
-                    <div className="double-buttons">
-                        <button>Enregistrer</button>
-                        <button className="optional">Annuler</button>
-                    </div>
+                <div className="block">
+                    <label>Titre</label>
+                    <input type="text" placeholder={props.taskInfos.Title} />
                 </div>
-            )
+                <div className="block">
+                    <label>Échéance</label>
+                    <input type="text" placeholder={props.taskInfos.DeadLine} />
+                </div>
+                <div className="block">
+                    <label>Notes</label>
+                    <textarea placeholder={props.taskInfos.Description}></textarea>
+                </div>
+                <div className="double-buttons">
+                    <button>Enregistrer</button>
+                    <button className="optional">Annuler</button>
+                </div>
+            </div>
+        )
     
    
 }
--- ProjetTuteureClient/src/components/TasksList.js
+++ ProjetTuteureClient/src/components/TasksList.js
@@ -45,85 +45,48 @@ function TasksList (props){
       setSelectedTask("");
     }else{
       setSelectedTask(index);
-      //request()
     }
     
   }
-/*
-  async function request(){
-    let parameters = new URLSearchParams()
-    parameters.append("IdTask",tasksInfos[selectedTask].IdTask);
-
-    const options = {
-      method: 'POST',
-      body: parameters
-    }
-    const reponse = await fetch('http://localhost:80/ProjetTuteureServer/get_steps', options)
-    const data = await reponse.json()
-    if(reponse.status === 200){
-      setStepsInfos(data.steps)
-    }
-    if(reponse.status === 201){
-      setStepsInfos("")
-      setError(data.message)
-    }else{
-      return <p className="error">{error}</p>
-    }
-  }
-
-
-  */
-
   
-  if(error !== ""){
+  if(selectedTask === ""){
     return (
       <div id="content">
         <div className="wrapper-tasks-list content-full">
-            <h1>Taches</h1>
-            <ul id="tasks-list">
-            <p>{error}</p>
-            <li>
-                <AddTask />
-            </li>
-            </ul></div>
-      </div>
-  )
-  }else if(selectedTask === ""){
-    return (
-    <div id="content">
-          <div className="wrapper-tasks-list content-full">
-            <h1>Taches</h1>
-            <ul id="tasks-list">
-            {
-              json2array(tasksInfos).map((taskInfo, index) => 
-                <li key={index}><Task tasksInfos={taskInfo} onClick={() => {handleTaskClick(index)}}/></li>
-              )
-            }
-            <li>
-                <AddTask />
-            </li>
-            </ul>
-          </div>
+          <h1>Taches</h1>
+          <ul id="tasks-list">
+          {
+            json2array(tasksInfos).map((taskInfo, index) => 
+              <li key={index}><Task tasksInfos={taskInfo} onClick={() => {handleTaskClick(index)}}/></li>
+            )
+          }
+          <li>
+              <AddTask />
+          </li>
+          </ul>
         </div>
+      </div>
     )
-   
-          }return (
-            <div id="content">
-              <div className="wrapper-tasks-list">
-                <h1>Taches</h1>
-                <ul id="tasks-list">
-                {
-              json2array(tasksInfos).map((taskInfo, index) => 
-                <li key={index}><Task tasksInfos={taskInfo} onClick={() => {handleTaskClick(index)}}/></li>
-              )
-            }
-                <li>
-                    <AddTask />
-                </li>
-                </ul>
-              </div>
-            </div>
-          ) 
+   }else{
+     return (
+      <div id="content">
+        <div className="wrapper-tasks-list">
+          <h1>Taches</h1>
+          <ul id="tasks-list">
+          {
+            json2array(tasksInfos).map((taskInfo, index) => 
+              <li key={index}><Task tasksInfos={taskInfo} onClick={() => {handleTaskClick(index)}}/></li>
+            )
+          }
+          <li>
+              <AddTask />
+          </li>
+          </ul>
+        </div>
+        <EditMenu taskInfos={tasksInfos[selectedTask]} />
+      </div>
+    ) 
+  }
 }
 
 export default TasksList
